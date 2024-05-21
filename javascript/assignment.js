cea3d854d811043acfc87b4d9f3829a4f47f0c92
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-05-21 18:35:02 +0500

    Add files via upload

---
+++ javascript/assignment.js
@@ -0,0 +1,56 @@
+                                                     //CHAPTER NO:1 ALERT
+
+//question no:1 
+let userName = prompt("Enter your name");
+if (userName !=null && userName != ""){
+    alert("Hello"+" "+userName+" "+"welcom to our website");
+}
+else{
+    alert("Hello userName welcom to our website");
+}
+//question no:2 
+alert("Error! please enter a valid password.");
+//question no:3
+alert("Welcome to JS Land... \n Happy coding");
+//question no:4
+//first task
+alert("welcome to JS land");
+//second task
+alert("Happy Coding\n prevent this page from creatng aditional dialogs."); 
+//question no:5 
+ console.log(alert("Hello... I can run JS through my web browser's console"));
+
+                                              //CHAPTER NO:2 VARIABLE FOR STRING
+//question no:1.
+let username;
+//question no:2.
+let myName;
+myName = "mariyam";
+console.log(myName);
+//question no:3.
+let message;
+message = "Hello World";
+alert(message);
+//question no:4.
+let studentName = "Mariyam";
+let studentAge = 17;
+let studentCourse = "web and mobile app development";
+ alert("student's name is"+" "+studentName);
+ alert("student's age is"+" "+studentAge);
+ alert("student's course is"+" "+studentCourse);
+//question no:5.
+let sentence;
+sentence = "pizza\npizz\npiz\npi\np";
+ alert(sentence);
+//question no:6.
+ let email;
+email = "mariyamsaleem97@gmail.com";
+alert("my email address is"+ " "+email);
+//question no:7.
+ let book;
+ book = "A smater \n way to learn jvascript";
+ alert("I am trying to learn from the Book"+" "+book);
+//question no:8.
+document.write("yah! I can write HTML content through Javascript");
+//question no:9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+//Make use of alerts in your new/existing HTML & CSS project.
+let userName = prompt("Enter your name");
+if(userName != null && userName != ""){
+    alert("Asslam O Alaikum!"+" " +userName+" " +"Welcome to \n our batch 11 admision form");
+}
+else{
+    alert("Asslam O Alaikum! user Welcome to \n our batch 11 admision form");
+}
